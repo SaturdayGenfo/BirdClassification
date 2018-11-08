09442e2727877eb15d698a3fc3a08cc566d9048e
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-08 22:29:06 +0100

    Update model.py

--- model.py
+++ model.py
@@ -14,9 +14,9 @@ class Net(nn.Module):
         self.fc2 = nn.Linear(50, nclasses)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        x = F.relu(F.max_pool2d(self.conv3(x), 2))
+        x = F.leaky_relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.leaky_relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.leaky_relu(F.max_pool2d(self.conv3(x), 2))
         x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
+        x = F.leaky_relu(self.fc1(x))
         return self.fc2(x)
