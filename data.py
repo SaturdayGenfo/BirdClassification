7ea56b7820f3431386918da2a36940d9dffeb35e
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-09 19:04:14 +0100

    Update data.py

--- data.py
+++ data.py
@@ -9,6 +9,8 @@ import torchvision.transforms as transforms
 # the training set
 data_transforms = transforms.Compose([
     transforms.Resize((64, 64)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomAffine(10, shear=2),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
