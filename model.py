39b5f8fdb5aa7877bbab0699cda476e9d7f84471
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-09 16:09:15 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,22 +1,19 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision.models as models
+
 
 nclasses = 20 
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=5)
-        self.conv2 = nn.Conv2d(16, 128, kernel_size=5)
-        self.conv3 = nn.Conv2d(128, 20, kernel_size=5)
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, nclasses)
+        self.PreTrainedResNet = torchvision.models.resnet18(pretrained=True)
+        for param in self.PreTrainedResNet.parameters():
+            param.requires_grad = False
+        sel.num_ftrs = self.PreTrainedResNet.fc.in_features
+        self.PreTrainedResNet.fc = nn.Linear(num_ftrs, 50)
 
     def forward(self, x):
-        x = F.leaky_relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.leaky_relu(F.max_pool2d(self.conv2(x), 2))
-        x = F.leaky_relu(F.max_pool2d(self.conv3(x), 2))
-        x = x.view(-1, 320)
-        x = F.leaky_relu(self.fc1(x))
-        return self.fc2(x)
+        return self.PreTrainedResNet.forward(x)
