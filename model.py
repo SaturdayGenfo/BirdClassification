7a06e28305bb14f6621fa7654cdb88008c68d154
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-09 16:33:10 +0100

    Update model.py

--- model.py
+++ model.py
@@ -9,10 +9,11 @@ nclasses = 20
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.PreTrainedResNet = models.resnet18(pretrained=True)
+        self.PreTrainedResNet = models.resnet50(pretrained=True)
         for param in self.PreTrainedResNet.parameters():
             param.requires_grad = False
-        self.PreTrainedResNet.fc = nn.Linear(567, 50)
+        self.numfeatures = self.PreTrainedResNet.fc.in_features
+        self.PreTrainedResNet.fc = nn.Linear(self.numfeatures, 50)
 
     def forward(self, x):
         return self.PreTrainedResNet.forward(x)
