d76ea9cbedb727d6eb7f858892af4b786f2c36d6
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-09 22:30:22 +0100

    Update model.py

--- model.py
+++ model.py
@@ -14,7 +14,7 @@ class Net(nn.Module):
         ct = 0
         for name, child in self.PreTrainedResNet.named_children():
             ct += 1
-            if ct < 1:
+            if ct < 4:
                 for name2, params in child.named_parameters():
                     params.requires_grad = False
         self.PreTrainedResNet.avgpool = nn.AvgPool2d(2, stride=1)
