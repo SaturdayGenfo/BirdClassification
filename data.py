3ccbd2cb682c8cf9b16abf81444b993042d7017a
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-09 16:58:47 +0100

    Update data.py

--- data.py
+++ data.py
@@ -8,7 +8,7 @@ import torchvision.transforms as transforms
 # and normalize them to mean = 0 and standard-deviation = 1 based on statistics collected from
 # the training set
 data_transforms = transforms.Compose([
-    transforms.Resize((224, 224)),
+    transforms.Resize((64, 64)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
