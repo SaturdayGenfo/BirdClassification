d9e1024e3ec018ecfacab71310472a12b886a8a6
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-09 20:37:34 +0100

    Update model.py

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ nclasses = 20
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.PreTrainedResNet = models.resnet50(pretrained=True)
+        self.PreTrainedResNet = models.resnet18(pretrained=True)
         
         ct = 0
         for name, child in self.PreTrainedResNet.named_children():
