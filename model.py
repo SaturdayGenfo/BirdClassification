c81ac8be3c3ca01aba53190be113d31fbef906f3
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-09 16:55:51 +0100

    Update model.py

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ nclasses = 20
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.PreTrainedResNet = models.resnet50(pretrained=True)
+        self.PreTrainedResNet = models.resnet18(pretrained=True)
         for param in self.PreTrainedResNet.parameters():
             param.requires_grad = False
         self.numfeatures = self.PreTrainedResNet.fc.in_features
