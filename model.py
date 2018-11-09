bee2b45f67f8eaa4a1926eeb89af1913feaa2a7e
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-09 21:24:57 +0100

    Update model.py

--- model.py
+++ model.py
@@ -9,17 +9,18 @@ nclasses = 20
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.PreTrainedResNet = models.resnet18(pretrained=True)
+        self.PreTrainedVGG = models.vgg19_bn(pretrained=True)
         
         ct = 0
-        for name, child in self.PreTrainedResNet.named_children():
+        for name, child in self.PreTrainedVGG.named_children():
             ct += 1
             if ct < 2:
                 for name2, params in child.named_parameters():
                     params.requires_grad = False
-        self.PreTrainedResNet.avgpool = nn.AvgPool2d(2, stride=1)
-        self.numfeatures = self.PreTrainedResNet.fc.in_features
-        self.PreTrainedResNet.fc = nn.Linear(self.numfeatures, 50)
+        self.num_features = self.PreTrainedVGG.classifier[6].in_features
+        self.features = list(self.PreTrainedVGG.classifier.children())[:-1] # Remove last layer
+        self.features.extend([nn.Linear(num_features, nclasses)]) # Add our layer with 4 outputs
+        self.PreTrainedVGG.classifier = nn.Sequential(*features) 
 
     def forward(self, x):
-        return self.PreTrainedResNet.forward(x)
+        return self.PreTrainedVGG.forward(x)
