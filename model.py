ed9a07f90c8cf0e61067ebe46f4198a8f737409b
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-10 12:25:37 +0100

    Update model.py

--- model.py
+++ model.py
@@ -14,13 +14,19 @@ class Net(nn.Module):
         ct = 0
         for name, child in self.PreTrainedVGG.named_children():
             ct += 1
-            if ct < 2:
+            if ct < 5:
                 for name2, params in child.named_parameters():
                     params.requires_grad = False
-        self.num_features = self.PreTrainedVGG.classifier[6].in_features
-        self.features = [nn.Linear(64, 4096)] + list(self.PreTrainedVGG.classifier.children())[1:-1]
-        self.features.extend([nn.Linear(self.num_features, nclasses)]) # Add our layer with 4 outputs
-        self.PreTrainedVGG.classifier = nn.Sequential(*self.features) 
+        self.classifier = nn.Sequential(
+            nn.Linear(64, 2048),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(2048, 2048),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(2048, nclasses),
+        )
+        self.PreTrainedVGG.classifier = self.classifier
 
     def forward(self, x):
         return self.PreTrainedVGG.forward(x)
