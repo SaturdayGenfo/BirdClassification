8dd6251b64669ab1e9b0eb498e8979a670418410
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-10 12:31:32 +0100

    Update model.py

--- model.py
+++ model.py
@@ -14,17 +14,17 @@ class Net(nn.Module):
         ct = 0
         for name, child in self.PreTrainedVGG.named_children():
             ct += 1
-            if ct < 5:
+            if ct < 15:
                 for name2, params in child.named_parameters():
                     params.requires_grad = False
         self.classifier = nn.Sequential(
-            nn.Linear(2048, 64),
+            nn.Linear(2048, 4096),
             nn.ReLU(True),
             nn.Dropout(),
-            nn.Linear(64, 64),
+            nn.Linear(4096, 4096),
             nn.ReLU(True),
             nn.Dropout(),
-            nn.Linear(64, nclasses),
+            nn.Linear(4096, nclasses),
         )
         self.PreTrainedVGG.classifier = self.classifier
 
