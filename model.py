7f97939bd65ad707696e09fdc9b8a3eab8f28959
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-13 00:11:02 +0100

    Update model.py

--- model.py
+++ model.py
@@ -9,19 +9,19 @@ nclasses = 20
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.PreTrainedResNet = models.resnet101(pretrained=True)
+        self.PreTrainedResNet = models.resnet18()
         
-        
+        '''
         ct = 0
         for name, child in self.PreTrainedResNet.named_children():
             ct += 1
             if ct < 3:
                 for name2, params in child.named_parameters():
                     params.requires_grad = False
-        
+        '''
         self.PreTrainedResNet.avgpool = nn.AvgPool2d(2, stride=1)
         self.numfeatures = self.PreTrainedResNet.fc.in_features
-        self.PreTrainedResNet.fc = nn.Linear(self.numfeatures, 50)
+        self.PreTrainedResNet.fc = nn.Linear(self.numfeatures, 20)
 
     def forward(self, x):
         return self.PreTrainedResNet.forward(x)
