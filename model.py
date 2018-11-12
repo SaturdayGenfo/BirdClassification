a343967d5e2997d0a5cbe3b37d551c9dbef9c867
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-12 18:39:11 +0100

    Update model.py

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ class Net(nn.Module):
         ct = 0
         for name, child in self.PreTrainedResNet.named_children():
             ct += 1
-            if ct < 10:
+            if ct < 5:
                 for name2, params in child.named_parameters():
                     params.requires_grad = False
         
