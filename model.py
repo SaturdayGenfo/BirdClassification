0c9f2ff41bee2ee81aecff7c1e5237fb6c10d30b
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-13 10:51:53 +0100

    Update model.py

--- model.py
+++ model.py
@@ -9,13 +9,13 @@ nclasses = 20
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.PreTrainedResNet = models.resnet101(pretrained=True)
+        self.PreTrainedResNet = models.resnet18(pretrained=True)
         
         
         ct = 0
         for name, child in self.PreTrainedResNet.named_children():
             ct += 1
-            if ct < 3:
+            if ct < 2:
                 for name2, params in child.named_parameters():
                     params.requires_grad = False
         
