8eb2ca874de565c5334eac5cd10440c83f1f3b64
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-13 20:22:52 +0100

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -43,7 +43,7 @@ output_file = open(args.outfile, "w")
 output_file.write("Id,Category\n")
 for f in tqdm(os.listdir(test_dir)):
     if 'jpg' in f:
-        data = data_transforms(pil_loader(test_dir + '/' + f))
+        data = data_transforms['test'](pil_loader(test_dir + '/' + f))
         data = data.view(1, data.size(0), data.size(1), data.size(2))
         if use_cuda:
             data = data.cuda()
