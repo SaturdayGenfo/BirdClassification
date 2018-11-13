65f933bb0524ef462e1c466330f556314c47a29d
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-13 11:51:59 +0100

    Update data.py

--- data.py
+++ data.py
@@ -9,13 +9,13 @@ import torchvision.transforms as transforms
 # and normalize them to mean = 0 and standard-deviation = 1 based on statistics collected from
 # the training set
 data_transforms = transforms.Compose([
-    transforms.Resize((512, 512)),
+    transforms.Resize((128, 128)),
     transforms.ColorJitter(hue=.01, saturation=.08),
-    #transforms.RandomVerticalFlip(),
+    transforms.RandomVerticalFlip(),
     transforms.RandomHorizontalFlip(),
     transforms.RandomRotation(30, resample=PIL.Image.BILINEAR),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 
