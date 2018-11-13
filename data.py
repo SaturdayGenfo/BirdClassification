96626ad04b68e662091534a1954d4530e2663537
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-13 15:19:11 +0100

    Update data.py

--- data.py
+++ data.py
@@ -10,10 +10,10 @@ import torchvision.transforms as transforms
 # the training set
 data_transforms = transforms.Compose([
     transforms.Resize((64, 64)),
-    transforms.ColorJitter(hue=.01, saturation=.08),
-    transforms.RandomVerticalFlip(),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(30, resample=PIL.Image.BILINEAR),
+    #transforms.ColorJitter(hue=.01, saturation=.08),
+    #transforms.RandomVerticalFlip(),
+    #transforms.RandomHorizontalFlip(),
+    #transforms.RandomRotation(30, resample=PIL.Image.BILINEAR),
     transforms.ToTensor(),
     #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
