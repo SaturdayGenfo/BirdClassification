57ec67a5832099161dfd5d5512d8ba1ed643f685
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-13 22:58:21 +0100

    Update model.py

--- model.py
+++ model.py
@@ -11,13 +11,14 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.PreTrainedResNet = models.resnet18(pretrained=True)
         
-        
+        '''
         ct = 0
         for name, child in self.PreTrainedResNet.named_children():
             ct += 1
             if ct < 5:
                 for name2, params in child.named_parameters():
                     params.requires_grad = False
+        '''
         
         self.PreTrainedResNet.avgpool = nn.AvgPool2d(2, stride=1)
         self.numfeatures = self.PreTrainedResNet.fc.in_features
