ef7d3f16aad79e6c4c6a1fe5bc0bcebf09dbd811
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-13 12:07:52 +0100

    Update model.py

--- model.py
+++ model.py
@@ -15,13 +15,20 @@ class Net(nn.Module):
         ct = 0
         for name, child in self.PreTrainedResNet.named_children():
             ct += 1
-            if ct < 5:
+            if ct < 3:
                 for name2, params in child.named_parameters():
                     params.requires_grad = False
         
         self.PreTrainedResNet.avgpool = nn.AvgPool2d(2, stride=1)
         self.numfeatures = self.PreTrainedResNet.fc.in_features
-        self.PreTrainedResNet.fc = nn.Linear(4608, 20)
+        self.PreTrainedResNet.fc = nn.Sequential(
+            nn.Linear(4068, 4096),
+            nn.ReLU(True),
+            nn.Dropout(0.2),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Linear(4096, nclasses),
+        )
 
     def forward(self, x):
         return self.PreTrainedResNet.forward(x)
