3e7ae7e134e3a331d5738052b15f2c07d0cd2409
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-14 18:55:43 +0100

    Update model.py

--- model.py
+++ model.py
@@ -11,15 +11,12 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.model = models.resnet50(pretrained=True)
         self.model.fc = nn.Linear(self.model.fc.in_features, nclasses)
-        
-        '''
         ct = 0
         for name, child in self.PreTrainedResNet.named_children():
             ct += 1
-            if ct < 5:
+            if ct < 3:
                 for name2, params in child.named_parameters():
                     params.requires_grad = False
-        '''
 
     def forward(self, x):
         return self.model.forward(x)
