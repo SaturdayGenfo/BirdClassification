819a944ee027608854de7bda51f5dc9ace3cf74d
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-14 19:39:52 +0100

    Update model.py

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ nclasses = 20
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.model = models.resnet50(pretrained=True)
+        self.model = models.resnet18(pretrained=True)
         self.model.fc = nn.Linear(self.model.fc.in_features, nclasses)
         ct = 0
         for name, child in self.model.named_children():
