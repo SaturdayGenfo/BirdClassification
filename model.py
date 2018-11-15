b7373855fbcb30f8821e1c020afdbee07755c51f
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-15 23:09:50 +0100

    Update model.py

--- model.py
+++ model.py
@@ -11,14 +11,12 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.model = models.densenet121(pretrained=True)
         self.model.classifier = nn.Linear(self.model.classifier.in_features, nclasses)
-        '''
         ct = 0
         for name, child in self.model.named_children():
             ct += 1
-            if ct < 3:
+            if ct < 60:
                 for name2, params in child.named_parameters():
                     params.requires_grad = False
-        '''
 
     def forward(self, x):
         return self.model.forward(x)
