1c669f454133a8ff26d003261152cbae4cbcd224
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-17 16:23:14 +0100

    Update data.py

--- data.py
+++ data.py
@@ -17,17 +17,17 @@ data_transforms = {
     transforms.RandomHorizontalFlip(),
     #transforms.RandomRotation(4, resample=PIL.Image.BILINEAR),
     transforms.ToTensor(),
-    #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]),
     
     'val':transforms.Compose([
     #transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]),
     
     'test':transforms.Compose([
     #transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 }
     
 
