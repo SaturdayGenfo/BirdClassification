3b3ae7c9b2eea718ea7abd2505c87638b0df9de3
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-17 21:04:02 +0100

    Update model.py

--- model.py
+++ model.py
@@ -9,22 +9,7 @@ nclasses = 20
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.PreTrainedVGG = models.vgg19_bn(pretrained=True)
-        
-        ct = 0
-        for name, child in self.PreTrainedVGG.named_children():
-            ct += 1
-            if ct < 3:
-                for name2, params in child.named_parameters():
-                    params.requires_grad = False
-        self.classifier = nn.Sequential(
-            nn.Linear(2048, 4096),
-            nn.ReLU(True),
-            nn.Linear(4096, 4096),
-            nn.ReLU(True),
-            nn.Linear(4096, nclasses),
-        )
-        self.PreTrainedVGG.classifier = self.classifier
+        self.model = models.vgg16(pretrained=True, num_classes=nclasses)
 
     def forward(self, x):
-        return self.PreTrainedVGG.forward(x)
+        return self.model.forward(x)
