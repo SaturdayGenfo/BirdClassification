196bed2269547dcae0c01736644984c1dbbfe3f2
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-17 20:54:06 +0100

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import zipfile
 import os
+import PIL
 
 import torchvision.transforms as transforms
 
@@ -7,14 +8,27 @@ import torchvision.transforms as transforms
 # by default, we resize the images to 64 x 64 in size
 # and normalize them to mean = 0 and standard-deviation = 1 based on statistics collected from
 # the training set
-data_transforms = transforms.Compose([
-    transforms.Resize((64, 64)),
-    transforms.ColorJitter(hue=.02, saturation=.08),
+data_transforms = {
+    
+    'train' : transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ColorJitter(hue=.02, saturation=.06),
     transforms.RandomVerticalFlip(),
     transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(10, resample=PIL.Image.BILINEAR),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
-])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]),
+    
+    'val':transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]),
+    
+    'test':transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+}
+    
 
 
