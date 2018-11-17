48fd8d77e4c08acff6ed673a4ea35ab638cd949c
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-17 21:06:32 +0100

    Update model.py

--- model.py
+++ model.py
@@ -9,7 +9,9 @@ nclasses = 20
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.model = models.vgg16(pretrained=True, num_classes=nclasses)
+        self.pretrained = models.vgg16(pretrained=True)
+        self.model = models.vgg16(pretrained=False, num_classes=nclasses)
+        self.model.features = self.pretrained.features
 
     def forward(self, x):
         return self.model.forward(x)
