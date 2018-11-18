752ed76b848c1bff5b951d5a989a77c6d6e2ffb4
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-18 18:31:32 +0100

    Update model.py

--- model.py
+++ model.py
@@ -9,7 +9,16 @@ nclasses = 20
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.model = models.resnet50(pretrained=True)
+        self.model = models.vgg16(pretrained=True)
+        self.model.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, nclasses),
+        )
         self.model.fc = nn.Linear(self.model.fc.in_features, nclasses)
 
     def forward(self, x):
