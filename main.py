57c8a03421f24939783e13ae0cf7a4313e8272ca
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-18 01:50:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def validation():
         validation_loss, correct, len(val_loader.dataset),
         100. * correct / len(val_loader.dataset)))
 
-scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10, 30, 70], gamma=0.01)
+scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[7, 30, 70], gamma=0.8)
 for epoch in range(1, args.epochs + 1):
     scheduler.step()
     train(epoch)
