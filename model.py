a57fb925058e832748fe2b4d4ec3079f37a24425
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-19 19:32:04 +0100

    Update model.py

--- model.py
+++ model.py
@@ -9,9 +9,9 @@ nclasses = 20
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.model = models.inception_v3(pretrained=True)
-        self.model.AuxLogits.fc = nn.Linear(self.model.AuxLogits.fc.in_features, nclasses)
-        self.model.fc = nn.Linear(self.model.fc.in_features, nclasses)
+        #self.model = models.squeezenet(pretrained=True)
+        self.model = models.squeezenet1_0(pretrained=use_pretrained)
+        self.model.classifier[1] = nn.Conv2d(512, nclasses, kernel_size=(1,1), stride=(1,1))
         '''
         ct = 0
         for name, child in self.model.named_children():
