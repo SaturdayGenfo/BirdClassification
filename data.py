d7738d92f8e4c15e400e5faf9e5e6ca012064e71
SaturdayGenfo <leello.tadesse@gmail.com>
2018-11-20 21:38:50 +0100

    Update data.py

--- data.py
+++ data.py
@@ -12,10 +12,10 @@ data_transforms = {
     
     'train' : transforms.Compose([
     transforms.Resize((224, 224)),
-    transforms.ColorJitter(hue=.01, saturation=.08),
+    transforms.ColorJitter(hue=.00, saturation=.03),
     transforms.RandomVerticalFlip(),
     transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(5, resample=PIL.Image.BILINEAR),
+    transforms.RandomRotation(10, resample=PIL.Image.BILINEAR),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]),
     
